820d3e342612965dde5df0f5b621fd9adc01281a
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-28 09:32:00 +0200

    checkAge update

--- js/view_controller.js
+++ js/view_controller.js
@@ -22,9 +22,30 @@
 // Modul Eingabe | Test:
 
 // Modul: Business-Logic (Mapping) | Test:
-ausgabe(checkAge());
-function checkAge() {
-    return "test";
+// ausgabe(checkAge(2));
+// ausgabe(checkAge(6));
+// ausgabe(checkAge(15));
+// ausgabe(checkAge(20));
+// ausgabe(checkAge(132));
+// ausgabe(checkAge());
+
+function checkAge(age) {
+    switch (true) {
+        case (age>=0 && age<=5):
+            return "milch";
+    
+        case (age>=6 && age<=12):
+            return "saft";
+        
+        case (age>=13 && age<=17):
+            return "cola";
+    
+         case (age>=18) && (age<=130):
+            return "wein";
+
+        default:
+            return "Tee";
+    }
 }
 
 
