76dfb84f306ce0cbc16e1e1b7fda278b126effa9
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-28 11:54:54 +0200

    Event-Dispatcher ++

--- js/view_controller.js
+++ js/view_controller.js
@@ -9,22 +9,46 @@
     Bild austauschen :: check!
 */
 
+
 // Modul Ablaufsteuerung | Test:
-controller()
+//controller()
 function controller() {
-    ausgabe(updateImg(checkAge(4)));
+    ausgabe(updateImg(checkAge(getInput())));
 }
 
 
 // Trigger - Btn 
+let btn = document.getElementById("trigBtn");
+btn = addEventListener("click" ,actOnClick);
+
+
 // Trigger - Input
 
 
 // Event-Dispatcher
+function actOnClick() {
+    if (isInputValid()) {
+        controller();
+    } else {
+        ausgabe("Input nicht korrekt.")
+    }
+    
+}
+
 
 // Check auf korrekte Eingaben ...
 
+
+
+
 // Modul Eingabe | Test:
+//ausgabe(getImput());
+function getInput() {
+    let inputField = document.getElementsByName("eingabe")[0];
+    let age = parseInt(inputField.value);
+    return age;
+}
+
 
 // Modul: Business-Logic (Mapping) | Test:
 // ausgabe(checkAge(2));
@@ -36,25 +60,25 @@ function controller() {
 function checkAge(age) {
     switch (true) 
     {
-        case (age >= 0) && (age <= 5):
-            return "milch";
-        case (age >= 6) && (age <= 12):
-            return "saft";
-        case (age >= 13) && (age <= 17):
-            return "cola";
-        case (age >= 18) && (age <= 130):
-            return "wein";    
+        case (age >= data.milk.lower) && (age <= data.milk.upper):
+            return data.milk.bev;
+        case (age >= data.juice.lower) && (age <= data.juice.upper):
+            return data.juice.bev;
+        case (age >= data.cola.lower) && (age <= data.cola.upper):
+            return data.cola.bev;
+        case (age >= data.wine.lower) && (age <= data.wine.upper):
+            return data.wine.bev;    
         default:
-            return "tee";  
+            return data.default.bev;  
     }
 }
 
 
 // Modul: Bild aktualisieren | Test:
-// ausgabe(updateImg("cola"));
+//ausgabe(updateImg("cola"));
 function updateImg(imgName) {
     let img = document.getElementById("bevImg");
-    img.src = "./bilder/" + imgName + ".jpg";
+    img.src = gui.img.path + imgName + gui.img.ext;
     return imgName; // monitoring
 }
 
